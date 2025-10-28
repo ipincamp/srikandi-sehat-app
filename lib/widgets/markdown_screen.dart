a1f8f038b43b9697d66609e270786cd769974899
ipincamp <support@nur-arifin.my.id>
2025-10-28 20:36:45 +0700

    feat: Bump dependency version
    - Bump outdated dependency for flutter_markdown
    - Update flutter_dotenv & connectivity_plus
    - Update app manifest for platform web
    - Add back widget test

--- lib/widgets/markdown_screen.dart
+++ lib/widgets/markdown_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 
 class MarkdownScreen extends StatefulWidget {
   final String title;
